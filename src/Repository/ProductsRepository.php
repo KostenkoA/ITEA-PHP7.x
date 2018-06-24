7c76f8b31d01e3327134a443311aa61c9d8db24b
KostenkoA <kostenkoam1212@gmail.com>
2018-06-25 00:54:56 +0300

    Type hints, script type

---
+++ src/Repository/ProductsRepository.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repository;
+
+
+class ProductsRepository
+{
+    public function getAll(): array
+    {
+        return [];
+    }
+
+    public function count(array $filters): int
+    {
+        return 0;
+    }
+}
