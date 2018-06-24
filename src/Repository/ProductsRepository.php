67c6f398faf7d6e785d3c96128590a5fe1506b96
KostenkoA <kostenkoam1212@gmail.com>
2018-06-25 01:23:45 +0300

    Null coalesce operator

--- src/Repository/ProductsRepository.php
+++ src/Repository/ProductsRepository.php
@@ -6,11 +6,26 @@ namespace App\Repository;
 
 class ProductsRepository
 {
+    const DEFAULT_LIMIT = 50;
+
     public function getAll(): array
     {
         return [];
     }
 
+    public function getProductForSale(array $filter)
+    {
+//        $limit = null;
+//
+//        if (isset($filter['limit'])){
+//            $limit = $filter['limit'];
+//        }
+
+        $limit = $filter['limit'] ?? self::DEFAULT_LIMIT;
+
+        return $limit;
+    }
+    
     public function count(array $filters): int
     {
         return 0;
