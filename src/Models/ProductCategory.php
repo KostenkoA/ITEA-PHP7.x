a3b10f67ed5db516dbc8eea42909488c7190dcc0
kostenkoa <kostenkoam1212@gmail.com>
2019-10-08 10:05:42 +0300

    task/[rebase] add id & name for ProductCategory

--- src/Models/ProductCategory.php
+++ src/Models/ProductCategory.php
@@ -4,6 +4,45 @@ namespace App\Models;
 
 class ProductCategory
 {
-  // new edit
+    /**
+     * @var integer
+     */
+    private $id;
 
+    /**
+     * @var integer
+     */
+    private $name;
+
+    /**
+     * @return int
+     */
+    public function getName(): int
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param int $name
+     */
+    public function setName(int $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
 }
