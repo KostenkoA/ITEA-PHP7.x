810dbd8f8f62b05bc2d268e951ddc6142c4a5c5f
kostenkoa <kostenkoam1212@gmail.com>
2019-10-08 18:56:08 +0300

    commit for rebase

--- src/Models/ProductCategory.php
+++ src/Models/ProductCategory.php
@@ -4,45 +4,5 @@ namespace App\Models;
 
 class ProductCategory
 {
-    /**
-     * @var integer
-     */
-    private $id;
 
-    /**
-     * @var integer
-     */
-    private $name;
-
-    /**
-     * @return int
-     */
-    public function getName(): int
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param int $name
-     */
-    public function setName(int $name): void
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return int
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param int $id
-     */
-    public function setId(int $id): void
-    {
-        $this->id = $id;
-    }
 }
